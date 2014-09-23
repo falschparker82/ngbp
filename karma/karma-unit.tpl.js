2865389ecd217ef728e0baf3e72183ca787be74b
Dominik Hamann <dominik.hamann@magicinternet.de>
2014-09-23 10:50:50 +0200

    needed another config entry

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -18,7 +18,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-coffee-preprocessor', 'karma-phantomjs-launcher' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
